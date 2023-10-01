5ec8ee97d8c810dd302f1e850aefee858fd161ed
kevinramos2 <keramosl@unal.edu.co>
2023-10-01 13:47:28 -0500

    correccion parámetros en constructores

--- src/test/java/vehiculos/Automovil.java
+++ src/test/java/vehiculos/Automovil.java
@@ -7,7 +7,7 @@ public class Automovil extends Vehiculo{
 	
 	
 	//atributos heredados 
-	public Automovil(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso, String traccion, Fabricante fabricante, int puestos) {
+	public Automovil(String placa,String nombre,int precio,int peso, Fabricante fabricante,int puestos) {
 		super(placa, 4,100,nombre,precio,peso,"FWD",fabricante);
 		this.puestos = puestos;
 		Automovil.automoviles += 1;
--- src/test/java/vehiculos/Camioneta.java
+++ src/test/java/vehiculos/Camioneta.java
@@ -7,7 +7,7 @@ public class Camioneta extends Vehiculo{
 	
 	//atributos heredados
 
-	public Camioneta(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso, String traccion, Fabricante fabricante, boolean volco) {
+	public Camioneta(String placa,int puertas,String nombre,int precio,int peso, Fabricante fabricante, boolean volco) {
 		super(placa, puertas, 90,nombre,peso, precio,"4X4",fabricante);
 		this.volco = volco;
 		Camioneta.camionetas += 1;
